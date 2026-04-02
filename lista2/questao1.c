96e6c33d95ed5ae84f1a6085ad50b559acf4f2d5
IMonardez <igormonardez@id.uff.br>
2026-04-02 20:39:21 -0300

    Questao 1

--- lista2/questao1.c
+++ lista2/questao1.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<math.h>
 
 int teste(int n);
+
+int pow_int(int base, int expoente);
 
 int main(int argc, char const *argv[])
 {
@@ -18,8 +21,34 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
+int pow_int(int base, int expoente) {
+	int result = 1;
+	for(int i = 0; i< expoente; i++) {
+		result *= base;
+	}
+	return result;
+}
+
 int teste(int n) {
 
-	
+	int inicio = abs(n); 
+	int fim;
+
+	fim = n%100;
+
+	while(inicio >= 100) {
+		inicio /=10;
+	}
+
+	int numero_esperado = inicio*100 + fim;
+	int soma = inicio + fim;
+
+	int numero_comparado = pow_int(soma,2);
+
+
+	if(numero_comparado == numero_esperado) 
+		printf("1\n");
+	else
+		printf("0\n");
 
 }
