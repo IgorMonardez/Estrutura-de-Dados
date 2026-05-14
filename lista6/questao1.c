ba36919fda8f2f81af39e2108b79ad57fb49e28a
IMonardez <igormonardez@id.uff.br>
2026-05-14 13:50:04 -0300

    Correção de listas anteriores

--- lista6/questao1.c
+++ lista6/questao1.c
@@ -9,26 +9,23 @@ typedef struct ponto {
 int dentroRet (TPTO2D *inf, TPTO2D *sup, TPTO2D *p);
 
 int main(int argc, char const *argv[]) {
-	
-	
-	TPTO2D *ponto_sup = (TPTO2D *) malloc(sizeof(TPTO2D));
-	TPTO2D *ponto_inf = (TPTO2D *) malloc(sizeof(TPTO2D));
+
+	TPTO2D ponto_sup, ponto_inf, ponto;
 	printf("Defina o retângulo (x,y) inferior e superior.\n");
-	scanf(" %d %d %d %d", &(ponto_inf->x), &(ponto_inf->y), &(ponto_sup->x), &(ponto_sup->y));
+	scanf(" %d %d %d %d", &(ponto_inf.x), &(ponto_inf.y), &(ponto_sup.x), &(ponto_sup.y));
+	
+	printf("Retângulo: inf (%d,%d), sup (%d,%d)\n", ponto_inf.x, ponto_inf.y, ponto_sup.x, ponto_sup.y);
 
-	printf("Retângulo: inf (%d,%d), sup (%d,%d)\n", ponto_inf->x, ponto_inf->y, ponto_sup->x, ponto_sup->y);
-
-	TPTO2D *ponto = (TPTO2D *) malloc(sizeof(TPTO2D));
 	printf("Digite o ponto.\n");
-	scanf(" %d %d", &ponto->x, &ponto->y);
+	scanf(" %d %d", &ponto.x, &ponto.y);
 
-	int result = dentroRet(ponto_inf, ponto_sup, ponto);
+	int result = dentroRet(&ponto_inf, &ponto_sup, &ponto);
 
 	if(result) 
-		printf("Ponto(%d,%d) está dentro do retangulo.\n", ponto->x, ponto->y);
+		printf("Ponto(%d,%d) está dentro do retangulo.\n", ponto.x, ponto.y);
 	else
-		printf("Ponto(%d,%d) NÃO está dentro do retangulo.\n", ponto->x, ponto->y);
-		
+		printf("Ponto(%d,%d) NÃO está dentro do retangulo.\n", ponto.x, ponto.y);
+	
 
 	return 0;
 }
